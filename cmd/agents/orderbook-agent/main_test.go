64744936a04b355cc7314c5f1e6fa7b6e1f8e887
Ajit Pratap Singh <ajitpratapsingh@Ajits-Mac-mini-2655.local>
2026-02-14 01:08:44 +0530

    fix: goimports with local prefix grouping

--- cmd/agents/orderbook-agent/main_test.go
+++ cmd/agents/orderbook-agent/main_test.go
@@ -6,10 +6,11 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ajitpratap0/cryptofunk/internal/agents"
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/ajitpratap0/cryptofunk/internal/agents"
 )
 
 var (
